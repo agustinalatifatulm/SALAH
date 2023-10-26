7d90eb4f415cb4910b4af6be21c2bf2e44a801c9
agustinalatifatulm <149041312+agustinalatifatulm@users.noreply.github.com>
2023-10-26 13:00:03 +0700

    Add files via upload

---
+++ Tugas 1/MakhlukHidup.java
@@ -0,0 +1,153 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan1PBO;
+
+/**
+ *
+ * @author HP
+ */
+public class MakhlukHidup {
+   
+    private String nama;
+    private String Pengelompokan;
+    private String BentukDaun;
+    private String BentukAkar;
+    private String BentukBunga;
+    private String BentukUmbi;
+    private String TempatHidup;
+    private String CiriBatang;
+    private String CaraReproduksi;
+    
+    protected String Tumbuh(){
+        return "saya tumbuh menjalar";
+    
+}
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the Pengelompokan
+     */
+    public String getPengelompokan() {
+        return Pengelompokan;
+    }
+
+    /**
+     * @param Pengelompokan the Pengelompokan to set
+     */
+    public void setPengelompokan(String Pengelompokan) {
+        this.Pengelompokan = Pengelompokan;
+    }
+
+    /**
+     * @return the BentukDaun
+     */
+    public String getBentukDaun() {
+        return BentukDaun;
+    }
+
+    /**
+     * @param BentukDaun the BentukDaun to set
+     */
+    public void setBentukDaun(String BentukDaun) {
+        this.BentukDaun = BentukDaun;
+    }
+
+    /**
+     * @return the BentukAkar
+     */
+    public String getBentukAkar() {
+        return BentukAkar;
+    }
+
+    /**
+     * @param BentukAkar the BentukAkar to set
+     */
+    public void setBentukAkar(String BentukAkar) {
+        this.BentukAkar = BentukAkar;
+    }
+
+    /**
+     * @return the BentukBunga
+     */
+    public String getBentukBunga() {
+        return BentukBunga;
+    }
+
+    /**
+     * @param BentukBunga the BentukBunga to set
+     */
+    public void setBentukBunga(String BentukBunga) {
+        this.BentukBunga = BentukBunga;
+    }
+
+    /**
+     * @return the BentukUmbi
+     */
+    public String getBentukUmbi() {
+        return BentukUmbi;
+    }
+
+    /**
+     * @param BentukUmbi the BentukUmbi to set
+     */
+    public void setBentukUmbi(String BentukUmbi) {
+        this.BentukUmbi = BentukUmbi;
+    }
+
+    /**
+     * @return the TempatHidup
+     */
+    public String getTempatHidup() {
+        return TempatHidup;
+    }
+
+    /**
+     * @param TempatHidup the TempatHidup to set
+     */
+    public void setTempatHidup(String TempatHidup) {
+        this.TempatHidup = TempatHidup;
+    }
+
+    /**
+     * @return the CiriBatang
+     */
+    public String getCiriBatang() {
+        return CiriBatang;
+    }
+
+    /**
+     * @param CiriBatang the CiriBatang to set
+     */
+    public void setCiriBatang(String CiriBatang) {
+        this.CiriBatang = CiriBatang;
+    }
+
+    /**
+     * @return the CaraReproduksi
+     */
+    public String getCaraReproduksi() {
+        return CaraReproduksi;
+    }
+
+    /**
+     * @param CaraReproduksi the CaraReproduksi to set
+     */
+    public void setCaraReproduksi(String CaraReproduksi) {
+        this.CaraReproduksi = CaraReproduksi;
+    }
+}
